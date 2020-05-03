0f88f25e8fa192a3afbc6cbbbaeb79930436f12c
damiano.barbati <damiano.barbati@gmail.com>
2020-05-03 12:17:28 +0200

    le icone...

--- src/lol.js
+++ src/lol.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { CiaoIcon } from './icon.js';
 
 const App = () => (
-    <h1>Daie!</h1>
+    <h1>
+        <span>Daie!</span>
+        <CiaoIcon width={24} />
+    </h1>
 );
 
 render(<App />, document.getElementById('ciaone'));
 
-if (module)
+if (module && module.hot)
     module.hot.accept();
